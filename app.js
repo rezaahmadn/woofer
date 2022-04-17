d85bd5675ef3066622a5b85cd86290e4bc453793
rezaahmadn <rezaahmadn@gmail.com>
2022-04-17 11:32:50 +0700

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const route = require('./routes')
 
 app.set('view engine', 'ejs')
